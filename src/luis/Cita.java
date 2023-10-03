8f6f1145db76316365f922fc12a9339ef35d62d5
IanMedinaB <paco-1204ian@hotmail.com>
2023-10-02 21:43:53 -0600

    Agrego leer datos de las citas

--- src/luis/Cita.java
+++ src/luis/Cita.java
@@ -1,8 +1,21 @@
 package luis;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Cita {
     public LocalDate fechaCita;
     public String motivo;
+
+    public static Cita leerDatos() {
+        Scanner s = new Scanner(System.in);
+        Cita c = new Cita();
+
+        System.out.println("Motivo de la cita");
+        c.motivo = s.nextLine();
+        System.out.println("Fecha de la cita (AAAA-MM-DD)");
+        c.fechaCita = LocalDate.parse(s.nextLine());
+
+        return c;
+    }
 }
