a50b23441a14166b7b621ed6f9753bc1839e0d02
MarcoIGH <marco.garcia.hurtado20@gmail.com>
2023-10-02 21:47:57 -0600

    Se realiza los cambios correspondientes en Cita y Dueño

--- src/luis/Cita.java
+++ src/luis/Cita.java
@@ -1,8 +1,24 @@
 package luis;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Cita {
     public LocalDate fechaCita;
     public String motivo;
+
+    public static Cita leerDatos() {
+        Scanner s = new Scanner(System.in);
+        Cita cita = new Cita();
+        System.out.println("Teclea la fecha de tu cita (AAAA-MM-DD):");
+        cita.fechaCita = LocalDate.parse(s.nextLine());
+        System.out.println("Teclea el motivo de tu cita:");
+        cita.motivo = s.nextLine();
+        return cita;
+    }
+
+    public static void main(String[] args) {
+        Cita cita = leerDatos();
+        System.out.println("Cita programada para el " + cita.fechaCita + " por el motivo: " + cita.motivo);
+    }
 }
