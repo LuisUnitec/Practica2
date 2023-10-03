0d11efd21a8583ec236055f77871eb242a073fd9
DanielSalazarGarciaUNITEC <daniel.salazargar@my.unitec.edu.mx>
2023-10-02 21:43:38 -0600

    cambios de cita dueño, agregado de biblioteca json y override de dueño

--- src/luis/Cita.java
+++ src/luis/Cita.java
@@ -1,8 +1,23 @@
 package luis;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Cita {
     public LocalDate fechaCita;
     public String motivo;
+
+    public static Cita leerDatos(){
+        Scanner s = new Scanner(System.in);
+        Cita c = new Cita();
+        System.out.println("Ingresa la fecha de tu cita formato AAAA-MM-DD");
+        c.fechaCita = LocalDate.parse(s.nextLine());
+        System.out.println("Ingresa el motivo de tu cita");
+        c.motivo = s.nextLine();
+
+        return c;
+    }
+
+
 }
